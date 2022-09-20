2fdba0cc012568388d79f68e50be1606d3d31b5c
Ric254 <eerickmwangi@gmail.com>
2022-09-19 17:47:50 -0700

    Write a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle line with hashtags
- * @size: an int given by main
- * Same as my 7 print diagonal. check that file and its comments
- * Description: Uses headers to link and a nested loops to achieve goal
- * can do one nested for instead of two. do a if j to i equality check
- * Return: void. no return.
+ * print_triangle - prints a triangle of size size
+ * @size: size of triangle
+ *
+ * Return: void
  */
-
 void print_triangle(int size)
 {
-	int i, j;
-	
+	int a, b;
+
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 0; i < size; i++)
+	for (a = 1; a <= size; a++)
 	{
-		for (j = size - i - 1; j > 0; j--)
-			_putchar(' ');
-		for (j = i + 1; j > 0; j--)
-			_putchar('#');
+		for (b = 1; b <= size; b++)
+		{
+			if (b <= (size - a))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
-
