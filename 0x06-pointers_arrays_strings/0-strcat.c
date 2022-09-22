9a4dab622b04c73271798abb32456ce3e24762af
Ric254 <eerickmwangi@gmail.com>
2022-09-22 12:22:12 -0700

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /** 
- * *_strcat - appends the src string to the dest string
+ * _strcat - appends the src string to the dest string
  *
  * @dest: char to check
  * @src: char to check
