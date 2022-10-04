0e6978bd18bdd59976e5ed20f323860d3cad2d78
Ric254 <eerickmwangi@gmail.com>
2022-10-04 08:15:53 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,70 +2,41 @@
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- *
- * Return: no return.
- */
-
-void ch_free_grid(char **grid, unsigned int height)
-{
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
-}
-
-/**
- * strtow - splites a string into words.
- * @str: string.
- *
- * Return: pointer of an array of integers.
- */
-
+* **strtow - function that splits a string into words.
+* @str : pointer
+* Return: Char
+*/
 char **strtow(char *str)
 {
-	char **aout;
-	unsigned int c, height, i, j, a1;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL)
 		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	aout = malloc((height + 1) * sizeof(char *));
-	if (aout == NULL || height == 0)
+
+	if (i = 0 ; str[i] != '\0' ; i++)
 	{
-		free(aout);
-		return (NULL);
+		if (str[i] != 32)
+			con++;
 	}
-	for (i = a1 = 0; i < height; i++)
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		for (c = a1; str[c] != '\0'; c++)
+		if (str[i] != 32)
 		{
-			if (str[c] == ' ')
-				a1++;
-			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			{
-				aout[i] = malloc((c - a1 + 2) * sizeof(char));
-				if (aout[i] == NULL)
-				{
-					ch_free_grid(aout, i);
-					return (NULL);
-				}
-					break;
-			}
+			*d[j] = str[i];
+			j++;
 		}
-		for (j = 0; a1 <= c; a1++, j++)
-			aout[i][j] = str[a1];
-		aout[i][j] = '\0';
+		else
+		{
+		}
 	}
-	aout[i] = NULL;
-	return (aout);
+	return (d);
 }
-Footer
-
