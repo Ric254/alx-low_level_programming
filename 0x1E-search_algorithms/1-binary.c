b318fb858cab07027c16d9d6a5a6585a642755dd
Ric254 <eerickmwangi@gmail.com>
2023-04-11 19:36:02 -0700

    Correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,33 +1,36 @@
 #include "search_algos.h"
 
 /**
- * binary_search - funct that searches for a value in a sorted array iterative
- * of integers using the Binary search algorithm
- * @array: Type pointer of given array
- * @size: Type size of elements in the array
- * @value: Type value to be searched
- * Return: Value, or -1 if value not present
+ * binary_search - performs binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int i, left, right;
+	size_t i = 0;
+	int *a = array;
 
 	if (!array)
 		return (-1);
-	for (left = 0, right = (int)size - 1; right >= left;)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
+	while (size)
+	{
+		for (i = 0, printf("Searching in array: "); i < size; i++)
+			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
+
+		i = (size - 1) / 2;
+		if (a[i] == value)
+			return ((a - array) + i);
+		else if (a[i] > value)
+			size = i;
 		else
-			left = i + 1;
+		{
+			a += (i + 1);
+			size -= (i + 1);
+		}
 	}
 	return (-1);
 }
